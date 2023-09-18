ef0989d2790e77a8b5431f7e548227dc3b760349
LingZhouChen <lzcciao@gmail.com>
2023-09-18 12:00:10 -0400

    new toy form

--- src/index.js
+++ src/index.js
@@ -14,8 +14,40 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-const toyName = document.getElementsByName('name')
-toyName.addEventListener ('submit', (e) => {
-  e.preventDefault()
-  
+// fetch toys
+// function fetchToys() {
+//   fetch(http://localhost:3000/toys)
+//       .then((response) => response.json())
+//       .then(renderToys)
+// }
+// fetchToys();
+
+const toyForm = document.querySelector('form')
+
+toyForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  newToy(e.target.name.value, e.target.image.value)
 })
+
+function newToy (toyName, toyImg) {
+  const div = document.createElement('div');
+  const toyheader = document.createElement('h2');
+  toyheader.innerText = toyName;
+
+  const img = document.createElement('img');
+  img.className = "toy-avatar";
+  img.src = toyImg;
+
+  const p = document.createElement('p');
+  p.innerText = ` 7 Likes`;
+
+
+  const btn = document.createElement('button');
+  btn.className = "like-btn";
+  btn.setAttribute('id', "[toy_id]");
+  btn.innerText = "Like ❤️";
+
+  const toyBox = document.querySelector('#toy-collection');
+  toyBox.append(div);
+  div.appendChild(img, p, btn);
+};
